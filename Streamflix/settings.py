5c3189dca23e75843abc39d296a550c58193a510
Rishav <rishavgairola0@gmail.com>
2023-06-22 00:21:11 +0530

    vercel

--- Streamflix/settings.py
+++ Streamflix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9ba^p-dvzo-)l88n@kcx1r7&1a5-yw8bxw*qowwdq%bnnfklmv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL ='static/'
+STATIC_URL ='/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -125,7 +125,4 @@ STATIC_URL ='static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static work
-import os
-
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
